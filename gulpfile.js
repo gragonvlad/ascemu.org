7c76ca0dcae7b9db53c675cadc8e0fc585e46125
Rachael Hepworth <evairfairy@gmail.com>
2016-12-17 11:45:39 +0000

    Remove bootstrap, jquery
    Add bulma

--- gulpfile.js
+++ gulpfile.js
@@ -15,5 +15,7 @@ require('laravel-elixir-vue-2');
 
 elixir((mix) => {
     mix.sass('app.scss')
-       .webpack('app.js');
+    	.version('css/app.css')
+    	.webpack('app.js')
+    	.version('js/app.js');
 });
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,11 +1,14 @@
 <html>
 <head>
     <title>{{ config('app.name', 'AscEmu') }}</title>
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}" />
 </head>
 <body>
     <div id="root">
         <h1>Welcome to AscEmu</h1>
-        <p>This is a temporary page.</p>
+        <p>This is a temporary page, and the time now is {{ date("h:i:sa") }}</p>
     </div>
+    
+    <script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
 </body>
 </html>
