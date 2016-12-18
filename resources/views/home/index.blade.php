d9afd76fde8bbcae3fbff6c825323bac116a38f7
Rachael Hepworth <evairfairy@gmail.com>
2016-12-18 08:49:15 +0000

    Add more demo text to demonstrate deployment

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,6 +14,17 @@
                 <br />
                 You can help us by <strong>contributing</strong>. It is completely open source and can be used by everyone.
             </p>
+            <h1 class="subtitle">
+                Our Technology
+            </h1>
+            <p>
+                AscEmu is built on a variety of technologies, including:
+                <ul>
+                    <li>C++</li>
+                    <li>Lua</li>
+                    <li>SQL</li>
+                </ul>
+            </p>
         </div>
     </div>
 @endsection
